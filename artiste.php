13d1de2547b1a2b6bcbfe55f3955a65f8150ef25
samuel <samuel.anneheim@outlook.fr>
2021-03-25 19:53:42 +0100

    Page artiste sql / modification dossier image

--- artiste.php
+++ artiste.php
@@ -4,235 +4,67 @@
     $title = "Aurelien vilette";
     require __DIR__.'./partials/navbar.php';
 
+    global $db;
+    $query = $db->prepare('SELECT * FROM oeuvre WHERE user_id = :id');
+    $query->bindValue(':id', 1);
+    $query->execute();
+    $oeuvres = $query->fetchAll();
+
+    $query = $db->prepare('SELECT * FROM user WHERE id = :id');
+    $query->bindValue(':id', 1);
+    $query->execute();
+    $artiste = $query->fetch();
+
+
 ?>
 
         <header>
             <div class="nom-artiste">
-                <h1>Aurélien Vilette</h1>
+                <h1><?= $artiste["prenom"]." ".$artiste["nom"] ?></h1>
                 <span>France</span>
             </div>
             <div class="presentation-artiste">
-                <p>Au-delà d'un simple souci documentaire et de pérennisation, Aurélien Vilette s'intéresse à la
-                    description
-                    et à l'esthétisation de lieux en déshérence. Les jeux de lumières, les matières vieillissantes, les
-                    perspectives et structures architecturales sont mis en evidence et magnifiés. Le silence,
-                    omniprésent,
-                    paraît correspondre à un temps qui se serait tu et fait émanger de ces photographies une athmosphère
-                    énigmatique, questionnant le spectateur sur la vie passée et l'esprit de ces lieux.</p>
+                <p><?= $artiste["presentation"] ?></p>
             </div>
         </header>
         <div class="container-logo">
             <figure>
-                <a href=""><img src="./assets/img/pageArtiste/twitter.png" alt="logo-twitter"></a>
-                <a href=""><img src="./assets/img/pageArtiste/facebook.png" alt="logo-facebook"></a>
-                <a href=""><img src="./assets/img/pageArtiste/google-plus.png" alt="logo-gmail-plus"></a>
-                <a href=""><img src="./assets/img/pageArtiste/pinterest.png" alt="logo-pinterest"></a>
+
+            <?php if(!is_null($artiste["twitter"])){ ?>
+                <a href="<?= $artiste["twitter"] ?>"><img src="./assets/img/reseauxSociaux/twitter.png" alt="logo-twitter"></a>
+            <?php } ?>
+
+            <?php if(!is_null($artiste["facebook"])){ ?>
+                <a href="<?= $artiste["facebook"] ?>"><img src="./assets/img/reseauxSociaux/facebook.png" alt="logo-facebook"></a>
+            <?php } ?>
+
+            <?php if(!is_null($artiste["instagram"])){ ?>
+                <a href="<?= $artiste["instagram"] ?>"><img src="./assets/img/reseauxSociaux/instagram.png" alt="logo-instagram"></a>
+            <?php } ?>
+
+            <?php if(!is_null($artiste["pinterest"])){ ?>
+                <a href="<?= $artiste["pinterest"] ?>"><img src="./assets/img/reseauxSociaux/pinterest.png" alt="logo-pinterest"></a>
+            <?php } ?>
+
             </figure>
         </div>
         <section>
+        <?php foreach ($oeuvres as $oeuvre) { ?>
+            
+        
             <article>
-                <a href="./article.php">
+                <a href="./article.php?=<?= $oeuvre["id"] ?>">
                     <figure>
-                        <img src="./assets/img/pageArtiste/verticalite-volute-ii-pologne.jpg" alt="verticalite-volute-ii-pologne">
+                        <img src="./assets/img/artiste/aurelien-vilette/<?= $oeuvre["image"] ?>" alt="verticalite-volute-ii-pologne">
                         <figcaption>
-                            <span class="titre-oeuvre">Verticalite Volute Ii Pologne</span>
-                            <p>à partir de <span class="prix">84,00€</span></p>
+                            <span class="titre-oeuvre"><?= $oeuvre["nom"] ?></span>
+                            <p>à partir de <span class="prix"><?= $oeuvre["prix"] ?>€</span></p>
                         </figcaption>
                     </figure>
                 </a>
             </article>
-            <article>
-                <a href="./article.php">
-                    <figure>
-                        <img src="./assets/img/pageArtiste/hotel-seventies-japan.jpg" alt="hotel-seventies-japan">
-                        <figcaption>
-                            <span class="titre-oeuvre">Hotel Seventies Japan</span>
-                            <p>à partir de <span class="prix">84,00€</span></p>
-                        </figcaption>
-                    </figure>
-                </a>
-            </article>
-            <article>
-                <a href="./article.php">
-                    <figure>
-                        <img src="./assets/img/pageArtiste/verticalite-volute-pologne.jpg" alt="verticalite-volute6pologne">
-                        <figcaption>
-                            <span class="titre-oeuvre">Verticalite Volute Pologne</span>
-                            <p>à partir de <span class="prix">89,00€</span></p>
-                        </figcaption>
-                    </figure>
-                </a>
-            </article>
-            <article>
-                <a href="./article.php">
-                    <figure>
-                        <img src="./assets/img/pageArtiste/erosions-venitiennes-ii.jpg" alt="erosions-venitiennes-ii">
-                        <figcaption>
-                            <span class="titre-oeuvre">Erosions Venitiennes Ii</span>
-                            <p>à partir de <span class="prix">89,00€</span></p>
-                        </figcaption>
-                    </figure>
-                </a>
-            </article>
-            <article>
-                <a href="./article.php">
-                    <figure>
-                        <img src="./assets/img/pageArtiste/topophilia-la-villa-rose-liban.jpg" alt="topophilia-la-villa-rose-liban">
-                        <figcaption>
-                            <span class="titre-oeuvre">Topophilia La Villa Rose Liban</span>
-                            <p>à partir de <span class="prix">84,00€</span></p>
-                        </figcaption>
-                    </figure>
-                </a>
-            </article>
-            <article>
-                <a href="./article.php">
-                    <figure>
-                        <img src="./assets/img/pageArtiste/au-dela.jpg" alt="au-dela">
-                        <figcaption>
-                            <span class="titre-oeuvre">Au-Dela</span>
-                            <p>à partir de <span class="prix">89,00€</span></p>
-                        </figcaption>
-                    </figure>
-                </a>
-            </article>
-            <article>
-                <a href="./article.php">
-                    <figure>
-                        <img src="./assets/img/pageArtiste/la-bicyclette-italie.jpg" alt="la-bicyclette-italie">
-                        <figcaption>
-                            <span class="titre-oeuvre">La Bicyclette Italie</span>
-                            <p>à partir de <span class="prix">89,00€</span></p>
-                        </figcaption>
-                    </figure>
-                </a>
-            </article>
-
-            <article>
-                <a href="./article.php">
-                    <figure>
-                        <img src="./assets/img/pageArtiste/reminiscence-le-buffet.jpg" alt="reminiscence-le-buffet">
-                        <figcaption>
-                            <span class="titre-oeuvre">Reminiscence Le Buffet</span>
-                            <p>à partir de <span class="prix">89,00€</span></p>
-                        </figcaption>
-                    </figure>
-                </a>
-            </article>
-            <article>
-                <a href="./article.php">
-                    <figure>
-                        <img src="./assets/img/pageArtiste/cuba-verticalite.jpg" alt="cuba-verticalite">
-                        <figcaption>
-                            <span class="titre-oeuvre">cuba Verticalite</span>
-                            <p>à partir de <span class="prix">89,00€</span></p>
-                        </figcaption>
-                    </figure>
-                </a>
-            </article>
-            <article>
-                <a href="./article.php">
-                    <figure>
-                        <img src="./assets/img/pageArtiste/reminiscence-livres-a-eux-memes.jpg"
-                            alt="reminiscence-livres-a-eux-memes">
-                        <figcaption>
-                            <span class="titre-oeuvre">Reminiscence Livré A Eux Mêmes</span>
-                            <p>à partir de <span class="prix">89,00€</span></p>
-                        </figcaption>
-                    </figure>
-                </a>
-            </article>
-            <article>
-                <a href="./article.php">
-                    <figure>
-                        <img src="./assets/img/pageArtiste/palazzo-sotto-voce.jpg" alt="palazzo-sotto-voce">
-                        <figcaption>
-                            <span class="titre-oeuvre">Palazzo Sotto Voce</span>
-                            <p>à partir de <span class="prix">89,00€</span></p>
-                        </figcaption>
-                    </figure>
-                </a>
-            </article>
-            <article>
-                <a href="./article.php">
-                    <figure>
-                        <img src="./assets/img/pageArtiste/verticalite-vue-sur-cour.jpg" alt="verticalite-vue-sur-cour">
-                        <figcaption>
-                            <span class="titre-oeuvre">Verticalite Vue sur Cour</span>
-                            <p>à partir de <span class="prix">89,00€</span></p>
-                        </figcaption>
-                    </figure>
-                </a>
-            </article>
-            <article>
-                <a href="./article.php">
-                    <figure>
-                        <img src="./assets/img/pageArtiste/erosions-venitiennes-ii.jpg" alt="erosions-venitiennes-ii">
-                        <figcaption>
-                            <span class="titre-oeuvre">Erosion Venitiennes Iv</span>
-                            <p>à partir de <span class="prix">89,00€</span></p>
-                        </figcaption>
-                    </figure>
-                </a>
-            </article>
-            <article>
-                <a href="./article.php">
-                    <figure>
-                        <img src="./assets/img/pageArtiste/lost-in-the-pines.jpg" alt="lost-in-the-pines">
-
-                        <figcaption>
-                            <span class="titre-oeuvre">Lost In The Pines</span>
-                            <p>à partir de <span class="prix">84,00€</span></p>
-                        </figcaption>
-                    </figure>
-                </a>
-            </article>
-            <article>
-                <a href="./article.php">
-                    <figure>
-                        <img src="./assets/img/pageArtiste/verticalite-centrale-hydraulique.jpg"
-                            alt="verticalite-centrale-hydraulique">
-                        <figcaption>
-                            <span class="titre-oeuvre">Verticalite Centrale Hydraulique</span>
-                            <p>à partir de <span class="prix">89,00€</span></p>
-                        </figcaption>
-                    </figure>
-                </a>
-            </article>
-            <article>
-                <a href="./article.php">
-                    <figure>
-                        <img src="./assets/img/pageArtiste/verticalite-la-ville-becarre-france.jpg"
-                            alt="verticalite-la-ville-becarre-france">
-                        <figcaption>
-                            <span class="titre-oeuvre">Verticalite La Ville Becarre France</span>
-                            <p>à partir de <span class="prix">84,00€</span></p>
-                        </figcaption>
-                    </figure>
-                </a>
-            </article>
-            <article>
-                <a href="./article.php">
-                    <figure>
-                        <img src="./assets/img/pageArtiste/palaciorubato.jpg" alt="palaciorubato">
-                        <figcaption>
-                            <span class="titre-oeuvre">Palacio Rubato</span>
-                            <p>à partir de <span class="prix">89,00€</span></p>
-                        </figcaption>
-                    </figure>
-                </a>
-            </article>
-            <article>
-                <a href="./article.php">
-                    <figure>
-                        <img src="./assets/img/pageArtiste/villa-arioso.jpg" alt="villa-arioso">
-                        <figcaption>
-                            <span class="titre-oeuvre">Villa Arioso</span>
-                            <p>à partir de <span class="prix">89,00€</span></p>
-                        </figcaption>
-                    </figure>
-                </a>
-            </article>
+        <?php } ?>
+           
         </section>
 
 <?php 
